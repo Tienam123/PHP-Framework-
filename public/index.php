739bd462f3d383e15ff135f7a9171f3e556f1fe9
Vladyslav <dr.tienam123@gmail.com>
2024-05-14 11:06:06 +0300

    Frameword version 1.0.2

--- public/index.php
+++ public/index.php
@@ -1,20 +1,12 @@
 <?php
 require_once dirname(__DIR__).'/vendor/autoload.php';
-define('BASE_PATH',dirname(__DIR__));
 
+define('BASE_PATH',dirname(__DIR__));
 
 use Somecode\Framework\Http\Kernel;
 use Somecode\Framework\Http\Request;
 use Somecode\Framework\Routing\Router;
 
 
-$request = Request::createFromGlobals();
-
-$router = new Router();
-
-$kernel = new Kernel($router);
-
-$responce = $kernel->handle($request);
-$responce->send();
 
 
