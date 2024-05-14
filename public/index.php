5637f7d9dec3749cd2bddc51a09a99acb3c5b9a0
Vladyslav <dr.tienam123@gmail.com>
2024-05-14 13:58:06 +0300

    Added folders

--- public/index.php
+++ public/index.php
@@ -2,11 +2,4 @@
 require_once dirname(__DIR__).'/vendor/autoload.php';
 
 define('BASE_PATH',dirname(__DIR__));
-
-use Somecode\Framework\Http\Kernel;
-use Somecode\Framework\Http\Request;
-use Somecode\Framework\Routing\Router;
-
-
-
-
+$var = 'Hello World!';
